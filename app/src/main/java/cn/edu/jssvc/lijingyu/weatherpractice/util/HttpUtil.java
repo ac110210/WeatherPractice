ec9634e07a47ab915bd899a1d67b4d609594d5be
ac110210 <969711814@qq.com>
2018-01-02 12:21:58 +0800

    完成遍历省市县三级列表的功能。

---
+++ app/src/main/java/cn/edu/jssvc/lijingyu/weatherpractice/util/HttpUtil.java
@@ -0,0 +1,16 @@
+package cn.edu.jssvc.lijingyu.weatherpractice.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by C515 on 2018/1/2.
+ */
+
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
